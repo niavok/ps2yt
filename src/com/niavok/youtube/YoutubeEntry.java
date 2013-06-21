0e28026f4b6837e38000a2f91d16f440be3bbfe1
Frédéric Bertolus <fred.bertolus@gmail.com>
2013-06-21 18:35:35 +0200

    Add licence header

--- src/com/niavok/youtube/YoutubeEntry.java
+++ src/com/niavok/youtube/YoutubeEntry.java
@@ -1,3 +1,21 @@
+/* 
+ * This file is part of PS2YT
+ *
+ * Copyright (C) 2013 Frédéric Bertolus (Niavok)
+ * 
+ * PS2YT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.niavok.youtube;
 
 import java.io.IOException;
@@ -74,9 +92,9 @@ public class YoutubeEntry {
             // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (SAXException e) {
-            throw new RessourceLoadingException("Failed to parse entry ", e);
+        	 e.printStackTrace();
         } catch (IOException e) {
-            throw new RessourceLoadingException("Failed to load entry", e);
+        	 e.printStackTrace();
         }	
 		return null;
 	}
